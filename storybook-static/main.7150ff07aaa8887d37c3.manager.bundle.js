3257d23684da1969742ff3dea7c45c85c4569923
Raihan Al Bazzy <36498349+excalibur995@users.noreply.github.com>
2021-10-03 09:44:39 +0700

    integrate with chromatic (#7)

---
+++ storybook-static/main.7150ff07aaa8887d37c3.manager.bundle.js
@@ -0,0 +1,21 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [1],
+  {
+    506: function (module, exports, __webpack_require__) {
+      __webpack_require__(507),
+        __webpack_require__(898),
+        __webpack_require__(906),
+        __webpack_require__(907),
+        __webpack_require__(903),
+        __webpack_require__(901),
+        __webpack_require__(900),
+        __webpack_require__(902),
+        __webpack_require__(899),
+        __webpack_require__(904),
+        __webpack_require__(905),
+        (module.exports = __webpack_require__(892));
+    },
+    574: function (module, exports) {},
+  },
+  [[506, 2, 3]],
+]);
