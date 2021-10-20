d98e2043dfa49948b61a24d4e83e0b85a11648b1
Bazzy <albazzlightyear@gmail.com>
2021-10-20 18:57:17 +0700

    update intro

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import Avatar from './components/Avatar';
 import Button from './components/Button';
+import Card from './components/Card';
+import Checkbox from './components/Checkbox';
 import Chips from './components/Chips';
 import Icon from './components/Icon';
+import RadioGroup from './components/RadioGroup';
 import Spinner from './components/Spinner';
 import Input from './components/Input';
 import Toggle from './components/Toggle';
@@ -9,16 +12,21 @@ import Typography from './components/Typography';
 // --------- miscellaneous ---------
 import { IdProvider } from '@radix-ui/react-id';
 import { getCssText } from './theme';
+import useToggle from './hooks/useToggle';
 export default {
   Avatar,
   Button,
+  Card,
+  Checkbox,
   Chips,
   Icon,
   Input,
+  RadioGroup,
   Spinner,
   Toggle,
   Typography,
   // --------- miscellaneous ---------
   getCssText,
+  useToggle,
   IdProvider,
 };
